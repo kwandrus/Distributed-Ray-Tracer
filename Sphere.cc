7d6973e4fb98b74e7bb079fe31d8cfd314c6a092
Kyle Andrus <kwandrus@ucdavis.edu>
2020-02-15 07:07:12 -0800

    continued organizing code

--- Sphere.cc
+++ Sphere.cc
@@ -15,7 +15,7 @@ Sphere::Sphere(Material* material, const Point& center, double radius, Vector mo
     if (motionVelocity != 0) {
         isMoving = true;
     }
-  inv_radius = 1./radius;
+    inv_radius = 1. / radius;
 }
 
 Sphere::~Sphere()
@@ -24,26 +24,28 @@ Sphere::~Sphere()
 
 void Sphere::getBounds(BoundingBox& bbox) const
 {
-  Vector diag(radius, radius, radius);
-  bbox.extend(center+diag);
-  bbox.extend(center-diag);
+    Vector diag(radius, radius, radius);
+    bbox.extend(center + diag);
+    bbox.extend(center - diag);
 }
 
 void Sphere::intersect(HitRecord& hit, const RenderContext&, const Ray& ray) const
 {
-  Vector O(ray.origin()-center);
-  const Vector& V(ray.direction());
-  double b = Dot(O, V);
-  double c = Dot(O, O)-radius*radius;
-  double disc = b*b-c;
-  if(disc > 0){
-    double sdisc = sqrt(disc);
-    double root1 = (-b - sdisc);
-    if(!hit.hit(root1, this, matl)){
-      double root2 = (-b + sdisc);
-      hit.hit(root2, this, matl);
+    Vector O(ray.origin() - center);
+    const Vector& V(ray.direction());
+    double b = Dot(O, V);
+    double c = Dot(O, O) - radius * radius;
+    double disc = b * b - c;
+
+    if(disc > 0){
+        double sdisc = sqrt(disc);
+        double root1 = (-b - sdisc);
+
+        if(!hit.hit(root1, this, matl)){
+            double root2 = (-b + sdisc);
+            hit.hit(root2, this, matl);
+        }
     }
-  }
 }
 
 void Sphere::move(double time) {
@@ -54,5 +56,5 @@ void Sphere::move(double time) {
 void Sphere::normal(Vector& normal, const RenderContext&, const Point& hitpos,
                     const Ray& ray, const HitRecord& hit) const
 {
-  normal = (hitpos-center)*inv_radius;
+    normal = (hitpos - center) * inv_radius;
 }
--- ThinLensCamera.cc
+++ ThinLensCamera.cc
@@ -16,33 +16,35 @@ ThinLensCamera::~ThinLensCamera()
 
 void ThinLensCamera::preprocess(double aspect_ratio)
 {
-  lookdir = lookat-eye;
-  focalLength = lookdir.length();
-  lookdir.normalize();
-  u = Cross(lookdir, up);
-  v = Cross(u, lookdir);
-  double ulen = tan(hfov/2.*M_PI/180.);
-  u.normalize();
-  u *= ulen;
-  double vlen = ulen/aspect_ratio;
-  v.normalize(); 
-  v *= vlen;
+    lookdir = lookat-eye;
+    focalLength = lookdir.length();
+    lookdir.normalize();
+    u = Cross(lookdir, up);
+    v = Cross(u, lookdir);
+    double ulen = tan(hfov/2.*M_PI/180.);
+    u.normalize();
+    u *= ulen;
+    double vlen = ulen/aspect_ratio;
+    v.normalize(); 
+    v *= vlen;
 }
 
 void ThinLensCamera::makeRay(Ray& ray, const RenderContext& context, double x, double y) const
 {
-    Vector direction = lookdir+u*x+v*y;
-    direction.normalize();
-    Point focalPoint = eye + focalLength * direction;
+    Vector rayDirection = lookdir + u * x + v * y;
+    rayDirection.normalize();
+    Point focalPoint = eye + focalLength * rayDirection;
 
+    // compute vector with random values between 0.0 and 1.0
     double randNum1 = (double(rand()) / double(RAND_MAX)) - 0.5;
     double randNum2 = (double(rand()) / double(RAND_MAX)) - 0.5;
     double randNum3 = (double(rand()) / double(RAND_MAX)) - 0.5;
     Vector shiftOrigin(randNum1, randNum2, randNum3);
 
+    // shift the origin of the ray from the camera by the aperture to produce depth of field effect
     Point newOrigin = eye + aperture * shiftOrigin;
-    direction = focalPoint - newOrigin;
-    direction.normalize();
+    rayDirection = focalPoint - newOrigin;
+    rayDirection.normalize();
 
-    ray = Ray(newOrigin, direction);
+    ray = Ray(newOrigin, rayDirection);
 }
